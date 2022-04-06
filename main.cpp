5af4c95105786e1ee24615f0d2de0fddef0ccb36
Deadlica <samuelgreenberg00@gmail.com>
2022-04-06 16:19:18 +0200

    Benchmarking

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main() {
     std::srand((unsigned) std::time(0));
     measurement benchmark(50, 20000);
 
-    benchmark(LINEAR_SEARCH, "Linear search.csv");
-    benchmark(BINARY_SEARCH, "Binary search.csv");
+    benchmark(LINEAR_SEARCH, "Linear search.txt");
+    benchmark(BINARY_SEARCH, "Binary search.txt");
     //benchmark(INTERPOLATION_SEARCH);
-    benchmark(BINARY_TREE_SEARCH, "Binary tree search.csv");
-    benchmark(HASH_SEARCH, "Hashtable search.csv");
+    benchmark(BINARY_TREE_SEARCH, "Binary tree search.txt");
+    benchmark(HASH_SEARCH, "Hashtable search.txt");
 
     return 0;
 }
